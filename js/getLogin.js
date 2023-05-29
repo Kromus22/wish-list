764574e1f93810a984d24fdb4036cbe4e572fbd2
Kromus22 <kromus2@gmail.com>
2023-05-29 16:48:36 +0500

    finish modal. WIP

--- js/getLogin.js
+++ js/getLogin.js
@@ -1,3 +1,17 @@
-export const getLogin = () => {
+import { API_URL } from "./const.js"
 
+export const getLogin = async (token) => {
+  try {
+    response = await fetch(`${API_URL}/getLogin`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
+    });
+
+    const data = response.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
--- js/renderNavigation.js
+++ js/renderNavigation.js
@@ -35,11 +35,13 @@ export const renderNavigation = () => {
             body: JSON.stringify(credentials),
           });
 
+          console.log(response);
           if (response.ok) {
             const data = await response.json();
             localStorage.setItem(JWT_TOKEN_KEY, data.token);
             auth.login = data.login;
             router.setRouter(`user/${data.login}`);
+
 
             return true;
           } else {
@@ -60,7 +62,41 @@ export const renderNavigation = () => {
   });
 
   buttonLogin.addEventListener('click', () => {
+    renderModal({
+      title: 'Авторизация',
+      description: 'Введите ваши данные для входа в личный кабинет',
+      btnSubmit: 'Авторизоваться',
+      submitHandler: async (e) => {
+        const formData = new FormData(e.target);
+        const credentials = {
+          login: formData.get('login'),
+          password: formData.get('password'),
+        }
 
+        try {
+          const response = await fetch(`${API_URL}/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(credentials),
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem(JWT_TOKEN_KEY, data.token);
+            auth.login = data.login;
+            router.setRouter(`user/${data.login}`);
+
+            return true;
+          } else {
+            const { message = 'Неизвестная ошибка' } = await response.json();
+            console.log(message);
+            throw new Error(message);
+          }
+        } catch (error) {
+          alert(error.message);
+        }
+      }
+    })
   });
 
   nav.append(buttonSignUp, buttonLogin);
